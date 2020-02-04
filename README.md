52bc0d1a8024fc198d568aef7be560fc038bf2aa
vinesma <otaviocos_13@hotmail.com>
2020-02-04 18:35:26 -0300

    Script is now audio only, added the cron timing in README

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ These scripts are meant to run at certain times using a cron job.
 
 ### Triforce
 
+0 18 * * 3
+
 Used to download podcasts automagically from a playlist using the date of upload.
 
 Currently set to download triforce episodes but it could be repurposed for basically any other playlist.
--- triforce/triforceScript.sh
+++ triforce/triforceScript.sh
@@ -3,14 +3,15 @@
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 
 PLAYLIST="https://www.youtube.com/playlist?list=PLJwv6sN_mnF0QsOTcKlFDeyzwXMM0MWru"
-STARTAT='120'
+STARTAT="120"
+AUDIOFORMAT="mp3"
 
 filename="lastDate"
 while read -r line; do
     DATE="$line"
 done < "$filename"
 
-youtube-dl --playlist-start "$STARTAT" --dateafter "$DATE" "$PLAYLIST"
+youtube-dl -x --audio-format "$AUDIOFORMAT" --playlist-start "$STARTAT" --dateafter "$DATE" "$PLAYLIST"
 
 notify-send "triforceScript has ran!"
 
