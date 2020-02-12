e98ae71a9ae62c6bd6a4c825a0df6d15731d10ac
vinesma <otaviocos_13@hotmail.com>
2020-02-12 09:53:38 -0300

    Rewrote scripts to be bash compliant and minimize namespace collisions
    - Changed variable names to lowercase in batteryScript.sh and triforceScript.sh

--- triforce/triforceScript.sh
+++ triforce/triforceScript.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:usr/local/bin/youtube-dl:/bin/date"
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:usr/local/bin/youtube-dl:/bin/date:/usr/share/man/man2/"
 
-PLAYLIST="https://www.youtube.com/playlist?list=PLJwv6sN_mnF0QsOTcKlFDeyzwXMM0MWru"
-STARTAT="121"
-AUDIOFORMAT="mp3"
+playlist="https://www.youtube.com/playlist?list=PLJwv6sN_mnF0QsOTcKlFDeyzwXMM0MWru"
+start_at="121"
+audio_format="mp3"
 
 filename="lastDate"
 while read -r line; do
-    DATE="$line"
+    date="$line"
 done < "$filename"
 
-youtube-dl -x --download-archive archive.txt --audio-format "$AUDIOFORMAT" --playlist-start "$STARTAT" --dateafter "$DATE" "$PLAYLIST"
+youtube-dl -x --download-archive archive.txt --audio-format "$audio_format" --playlist-start "$start_at" --dateafter "$date" "$playlist"
 
 date +%Y%m%d > lastDate
