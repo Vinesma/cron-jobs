2551da3398e906d390945e907a40c30cc344fa91
vinesma <otaviocos_13@hotmail.com>
2020-02-28 09:12:40 -0300

    Added absolute paths to triforceScript, hopefully it will actually work.
    - Changed PATH into separate variables for 'youtube-dl' and 'date' paths.

--- triforce/triforceScript.sh
+++ triforce/triforceScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:usr/local/bin/youtube-dl:/bin/date:/usr/share/man/man2/"
+youtube_dl_path="/usr/local/bin/youtube-dl"
+date_path="/bin/date"
 
 playlist="https://www.youtube.com/playlist?list=PLJwv6sN_mnF0QsOTcKlFDeyzwXMM0MWru"
 start_at="121"
@@ -11,6 +11,6 @@ while read -r line; do
     date="$line"
 done < "$filename"
 
-youtube-dl -x --download-archive archive.txt --audio-format "$audio_format" --playlist-start "$start_at" --dateafter "$date" "$playlist"
+"$youtube_dl_path" -x --download-archive archive.txt --audio-format "$audio_format" --playlist-start "$start_at" --dateafter "$date" "$playlist"
 
-date +%Y%m%d > lastDate
+"$date_path" +%Y%m%d > lastDate
