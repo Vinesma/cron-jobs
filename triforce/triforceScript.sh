8665fe6d34053d373132e32ed13767d8bf11ab5c
vinesma <otaviocos_13@hotmail.com>
2020-03-04 20:56:05 -0300

    triforceScript has finally worked!
    - Added a notification to it

--- triforce/triforceScript.sh
+++ triforce/triforceScript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 youtube_dl_path="/usr/local/bin/youtube-dl"
 date_path="/bin/date"
+notify_send_path="/usr/bin/notify-send"
 
 playlist="https://www.youtube.com/playlist?list=PLJwv6sN_mnF0QsOTcKlFDeyzwXMM0MWru"
-start_at="121"
+start_at="123"
 audio_format="mp3"
 
 filename="lastDate"
@@ -14,3 +15,4 @@ done < "$filename"
 "$youtube_dl_path" -x --download-archive archive.txt --audio-format "$audio_format" --playlist-start "$start_at" --dateafter "$date" "$playlist"
 
 "$date_path" +%Y%m%d > lastDate
+"$notify_send_path" "CRON : triforceScript just ran!"
