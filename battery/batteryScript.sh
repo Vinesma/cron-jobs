9d84e7db62d139cc5f7cf6b7f49b51e7709c6fbf
Vinesma <otaviocos_13@hotmail.com>
2020-03-05 16:12:38 -0300

    Fix attribution error

--- battery/batteryScript.sh
+++ battery/batteryScript.sh
@@ -6,7 +6,7 @@ head_path="/usr/bin/head"
 
 "$upower_path" -i /org/freedesktop/UPower/devices/battery_BAT1 | "$grep_path" percentage: | "$grep_path" -oE [1-9]{2} > percentage
 
-current_percentage = "$head_path" percentage
+current_percentage=$("$head_path" percentage)
 
 if [ "$current_percentage" -le 50 ]
 then "$notify_send_path" "Battery power is getting low, save your work!"
