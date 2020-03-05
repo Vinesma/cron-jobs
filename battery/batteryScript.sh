c10faa3c47b99dfebafc4785ec535f061ccb4c32
Vinesma <otaviocos_13@hotmail.com>
2020-03-04 21:26:47 -0300

    batteryScript also finally works!
    - Same as triforceScript, just changed a few things.

--- battery/batteryScript.sh
+++ battery/batteryScript.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+upower_path="/usr/bin/upower"
+grep_path="/bin/grep"
+notify_send_path="/usr/bin/notify-send"
+head_path="/usr/bin/head"
 
-PATH="/usr/local/lib/nodejs/node-v12.14.1-linux-x64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
-upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep percentage: | grep -oE [1-9]{2} > percentage
+"$upower_path" -i /org/freedesktop/UPower/devices/battery_BAT1 | "$grep_path" percentage: | "$grep_path" -oE [1-9]{2} > percentage
 
-while read -r line; do
-	current_percentage="$line"
-done < percentage
+current_percentage = "$head_path" percentage
 
-if [ "$current_percentage" -le 45 ]
-then notify-send "Battery power is getting low, save your work!"
+if [ "$current_percentage" -le 50 ]
+then "$notify_send_path" "Battery power is getting low, save your work!"
 fi
