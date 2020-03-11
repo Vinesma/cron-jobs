090d0d5d27f135798cde6729db270758c9506d59
vinesma <otaviocos_13@hotmail.com>
2020-03-11 18:35:51 -0300

    Add output string pointing to a Sync folder in my network

--- triforce/triforceScript.sh
+++ triforce/triforceScript.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 export DISPLAY=:0.0
+home_path="/home/vinesma"
 youtube_dl_path="/usr/local/bin/youtube-dl"
 date_path="/bin/date"
 notify_send_path="/usr/bin/notify-send"
 
 playlist="https://www.youtube.com/playlist?list=PLJwv6sN_mnF0QsOTcKlFDeyzwXMM0MWru"
-start_at="123"
+start_at="126"
 audio_format="mp3"
+output_string="$home_path/Sync/%(title)s.%(ext)s"
 
 filename="lastDate"
 while read -r line; do
     date="$line"
 done < "$filename"
 
-"$youtube_dl_path" -x --download-archive archive.txt --audio-format "$audio_format" --playlist-start "$start_at" --dateafter "$date" "$playlist"
+"$youtube_dl_path" -x --download-archive archive.txt --audio-format "$audio_format" --playlist-start "$start_at" --dateafter "$date" -o "$output_string" "$playlist"
 
 "$date_path" +%Y%m%d > lastDate
 "$notify_send_path" "CRON : triforceScript just ran!"
