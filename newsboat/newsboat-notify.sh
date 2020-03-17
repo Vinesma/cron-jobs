f18d781c760fbba97dcab905fbae1e19557f5584
Vinesma <otaviocos_13@hotmail.com>
2020-03-17 15:08:12 -0300

    Remove redundant notify-send
    - newsboat already sends a notification

--- newsboat/newsboat-notify.sh
+++ newsboat/newsboat-notify.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 export DISPLAY=:0.0
 newsboat_path="/usr/bin/newsboat"
-notify_send_path="/usr/bin/notify-send"
 
 "$newsboat_path" -x reload
-"$notify_send_path" "CRON : Newsboat updated!"
